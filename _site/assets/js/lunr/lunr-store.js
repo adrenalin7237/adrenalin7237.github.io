b13a2e94cc302094ba95b933251b0fd69fc3dd6e
adrenalin7237 <adrenalin7237@gmail.com>
2021-06-15 16:14:51 +0900

    initialize blog

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "First post",
+        "excerpt":"Test     test 실시            제대로 upload 되는가       로컬에서도 확인 가능한가           ","categories": ["Blog"],
+        "tags": ["Blog","jekyll","Github","Git"],
+        "url": "/blog/first-post/",
+        "teaser": null
+      }]
