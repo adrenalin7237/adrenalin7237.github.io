3d7c96979a3e1c2fd31c536192d5cc023bfb1d49
adrenalin7237 <adrenalin7237@gmail.com>
2021-06-21 19:00:49 +0900

    ros2 properties

---
+++ _posts/2021-06-21-ros2-properties.md
@@ -0,0 +1,30 @@
+---
+title : "ROS2 중요 특징"
+excerpt: "ROS1과 비교하는 ROS2"
+
+categories:
+  - ROS2 foxy
+tags:
+  - [ROS2, foxy]
+
+toc: true
+toc_sticky: true
+
+date: 2021-06-21
+last_modified_at: 2020-06-21
+---
+# 참조사이트
+- 모든 정보는 [**이곳**](https://cafe.naver.com/openrt/23965)에서 참조하였다.
+
+# ROS1 vs ROS2
+
+| ROS1 | ROS2|
+|---:|---:
+|단일로봇|복수대의 로봇
+|워크스테이션급 컴퓨터|임베디드에서도 사용가능
+리눅스 환경|멀티 플랫폼(리눅스,맥,윈도우)
+실시간 제어 불가능 | 실시간 제어 가능
+안정된 네트워크 환경요구 | 불안정한 네트워크에서도 동작가능(유연)
+
+
+
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,4 +4,10 @@ var store = [{
         "tags": ["Blog","jekyll","Github","Git"],
         "url": "/blog/first-post/",
         "teaser": null
+      },{
+        "title": "ROS2 중요 특징",
+        "excerpt":"참조사이트     모든 정보는 이곳에서 참조하였다.   ROS1 vs ROS2                  ROS1       ROS2                       단일로봇       복수대의 로봇                 워크스테이션급 컴퓨터       임베디드에서도 사용가능                 리눅스 환경       멀티 플랫폼(리눅스,맥,윈도우)                 실시간 제어 불가능       실시간 제어 가능                 안정된 네트워크 환경요구       불안정한 네트워크에서도 동작가능(유연)           ","categories": ["ROS2 foxy"],
+        "tags": ["ROS2","foxy"],
+        "url": "/ros2%20foxy/ros2-properties/",
+        "teaser": null
       }]
